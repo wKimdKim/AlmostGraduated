acbe5c84630ecae18504dc1984575b369416610d
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-05-19 16:28:16 +1200

    stores lat and long when marker is placed

--- www/FlaskApp/FlaskApp/static/js/script.js
+++ www/FlaskApp/FlaskApp/static/js/script.js
@@ -1,4 +1,6 @@
-var marker;
+var lat;
+var long;
+
 function myMap() {
   var mapProp= {
       center:new google.maps.LatLng(-36.852701,174.7699),
@@ -6,29 +8,14 @@ function myMap() {
   };
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
   var mapListener = map.addListener('click', function(e) {
-    placeMarkerAndPanTo(e.latLng, map);
+    var marker = new google.maps.Marker()
+    marker.setPosition(e.latLng);
+    lat = marker.position.lat();
+    long = marker.position.lng();
+    placeMarkerAndPanTo(marker.getPosition(), map);
     google.maps.event.clearListeners(map, 'click');
   });
 }
-
-
-
-function Create_event() {
-	var Name = $("#name").val();
-	var Area = $("#area").val();
-	$.ajax({
-		url: '/add/event',
-		contentType: 'application/json',
-		type: 'POST',
-		data: JSON.stringify({'Name':Name,'Area':Area}),
-		success: function(response){
-			alert(response)
-			},
-		error: function(response){
-			alert(response)
-			}
-	});
-};
 
 function placeMarkerAndPanTo(latLng, map) {
   var marker = new google.maps.Marker({
@@ -37,3 +24,23 @@ function placeMarkerAndPanTo(latLng, map) {
   });
   map.panTo(latLng);
 }
+
+
+
+function Create_event() {
+	var Name = $("#name").val();
+	var Area = $("#area").val();
+
+	$.ajax({
+		url: '/add/event',
+		contentType: 'application/json',
+		type: 'POST',
+		data: JSON.stringify({'Name':Name,'Area':Area, 'Latitude': Latitude, 'Longitude': Longitude}),
+		success: function(response){
+			alert(response)
+			},
+		error: function(response){
+			alert(response)
+			}
+	});
+};
