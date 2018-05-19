6abab4cc79faba5935f0806b28091f0f983c1712
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-05-19 14:28:30 +1200

    Added sidebar

--- www/FlaskApp/FlaskApp/static/js/script.js
+++ www/FlaskApp/FlaskApp/static/js/script.js
@@ -1,7 +1,7 @@
 function myMap() {
   var mapProp= {
-      center:new google.maps.LatLng(51.508742,-0.120850),
-      zoom:5,
+      center:new google.maps.LatLng(-36.852701,174.7699),
+      zoom:18,
   };
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
 }
