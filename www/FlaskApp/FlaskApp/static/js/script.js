48637b95c6ae2ce7285491102ffc6de9fb34c548
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-05-20 14:14:54 +1200

    conflict

--- www/FlaskApp/FlaskApp/static/js/script.js
+++ www/FlaskApp/FlaskApp/static/js/script.js
@@ -2,7 +2,6 @@
 var marker;
 var latitude;
 var longitude;
-var Areas = ["OGGB Upstairs", "OGGB Downstairs", "Computer Science Ground Labs", "GenLib"];
 
 /*Creates the google map. Added a listener so when user click on the map, marker is placed.
 Saves the marker's latitude and longitude*/
@@ -49,65 +48,25 @@ function myMap() {
 
 function Create_event() {
   var Name = $("#name").val();
-  console.log(Name);
-  var Area = $("#exampleFormControlSelect1").val();
-  if (Area in Areas){
-    if (Area === Areas[0]){
-      lat = 174.771057;
-      long = -36.853074;
-    }
-    if (Area === Areas[1]){
-      lat = 174.771698;
-      long = -36.852552;
-    }
-    if (Area === Areas[2]){
-      lat = 174.767908;
-      long = -36.853063;
-    }
-    if (Area === Areas[3]){
-      lat = 174.76947;
-      long = -36.85137;
-    }
-  }
-  console.log(Area);
+  var Area = $("#area").val();
   var DateTime = $("#datetime-local").val();
   console.log(DateTime);
+  var lat = latitude;
+  var long = longitude;
 
-  if (Area === "Other"){
-    if (lat == null || long == null){
-      alert("Please choose a location on the map");
-    }
-    else{
-      var lat = latitude;
-      var long = longitude;
-      $.ajax({
-        url: '/add/event',
-        contentType: 'application/json',
-        type: 'POST',
-        data: JSON.stringify({'Name':Name,'Area':Area,'DateTime':DateTime, 'Latitude':lat, 'Longitude':long}),
-        success: function(response){
-          alert(response)
-          },
-        error: function(response){
-          alert(response)
-          }
-      });
-    }
-    else{
-      $.ajax({
-        url: '/add/event',
-        contentType: 'application/json',
-        type: 'POST',
-        data: JSON.stringify({'Name':Name,'Area':Area,'DateTime':DateTime, 'Latitude':lat, 'Longitude':long}),
-        success: function(response){
-          alert(response);
-          },
-        error: function(response){
-          alert(response);
-          }
-      });
-    }
-  }
+  $.ajax({
+    url: '/add/event',
+    contentType: 'application/json',
+    type: 'POST',
+    data: JSON.stringify({'Name':Name,'Area':Area,'DateTime':DateTime, 'Latitude': lat, 'Longitude': long}),
+    success: function(response){
+      alert(response)
+      console.log(response);
+      },
+    error: function(response){
+      alert(response)
+      }
+  });
 }
 
 function moreEventDetails() {
