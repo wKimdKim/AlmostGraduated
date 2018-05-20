dc80fd86a8db09f8453be63ce99cc1e458183189
Adrian Thomas <akrthomas@gmail.com>
2018-05-20 13:16:27 +1200

    Rearranged tags

--- www/FlaskApp/FlaskApp/static/js/script.js
+++ www/FlaskApp/FlaskApp/static/js/script.js
@@ -1,37 +1,80 @@
 /*Global Variables*/
 var marker;
-let latitude;
-let longitude;
+var latitude;
+var longitude;
 
 /*Creates the google map. Added a listener so when user click on the map, marker is placed.
 Saves the marker's latitude and longitude*/
 function myMap() {
-  var mapProp= {
-      center:new google.maps.LatLng(-36.852701,174.7699),
-      zoom:18,
-  };
-  var map=new google.maps.Map(document.getElementById("map"),mapProp);
-  var mapListener = map.addListener('click', function(e) {
-    if (marker){
-      marker.setPosition(e.latLng);
-    }
-    else{
-      marker = new google.maps.Marker({
-        position: e.latLng,
-        map: map
-      });
-    }
-    // console.log(marker.position.lng());
-    // marker = new google.maps.Marker()
-    // marker.setPosition(e.latLng);
-    latitude = marker.position.lat();
-    longitude = marker.position.lng();
-    // console.log(latitude);
-    // console.log(longitude);
-    // placeMarkerAndPanTo(marker.getPosition(), map);
-    // google.maps.event.clearListeners(map, 'click');
-  });
+    var mapProp = {
+        center: new google.maps.LatLng(-36.852701, 174.7699),
+        zoom: 18,
+        styles: [{
+            "featureType": "administrative",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}, {"hue": "#0066ff"}, {"saturation": 74}, {"lightness": 100}]
+        }, {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}]
+        }, {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}, {"weight": 0.6}, {"saturation": -85}, {"lightness": 61}]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{"visibility": "on"}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "all",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "road.local",
+            "elementType": "all",
+            "stylers": [{"visibility": "on"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}]
+        }, {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [{"visibility": "simplified"}, {"color": "#5f94ff"}, {"lightness": 26}, {"gamma": 5.86}]
+        }]
+
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var mapListener = map.addListener('click', function (e) {
+        if (marker) {
+            marker.setPosition(e.latLng);
+        }
+        else {
+            marker = new google.maps.Marker({
+                position: e.latLng,
+                map: map
+            });
+        }
+        // console.log(marker.position.lng());
+        // marker = new google.maps.Marker()
+        // marker.setPosition(e.latLng);
+        latitude = marker.position.lat();
+        longitude = marker.position.lng();
+        // console.log(latitude);
+        // console.log(longitude);
+        // placeMarkerAndPanTo(marker.getPosition(), map);
+        // google.maps.event.clearListeners(map, 'click');
+    });
 }
+
 // google.maps.event.addListener(map, 'click', function(event) {
 //   placeMarker(event.latLng);
 // });
@@ -44,41 +87,48 @@ function myMap() {
 //   map.panTo(latLng);
 // }
 
+function showModal(message) {
+
+}
 
 
 function Create_event() {
-  var Name = $("#name").val();
-  var Area = $("#exampleFormControlSelect1").val();
-  var DateTime = $("#datetime-local").val();
-  
-  
-  if(Area=='Other' && latitude!='' && longitude!='')
-  {
-    alert('Select a location');
-    return;
-  }
+    var Name = $("#name").val();
+    var Area = $("#exampleFormControlSelect1").val();
+    var DateTime = $("#datetime-local").val();
 
-  if(Name=='' || DateTime=='')
-  {
-    alert("Please enter a Name and Time");
-    return;
-  }
 
-  var email = $('#email').val();
-  var description = $('#description').val()
+    if (Area == 'Other' && latitude != '' && longitude != '') {
 
-  $.ajax({
-    url: '/add/event',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({'Name':Name,'Area':Area,'DateTime':DateTime, 'Email': email, 'Description': description}),
-    success: function(response){
-      console.log(response);
-      },
-    error: function(response){
-      alert(response)
-      }
-  });
+        alert('Select a location');
+    }
+
+    if (Name == '' || DateTime == '') {
+        alert("Please enter a Name and Time");
+        return;
+    }
+
+    var email = $('#email').val();
+    var description = $('#description').val()
+
+    $.ajax({
+        url: '/add/event',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify({
+            'Name': Name,
+            'Area': Area,
+            'DateTime': DateTime,
+            'Email': email,
+            'Description': description
+        }),
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (response) {
+            alert(response)
+        }
+    });
 }
 
 function moreEventDetails() {
@@ -91,18 +141,18 @@ function moreEventDetails() {
 }
 
 
-function get_marker(id){
-  $.ajax({
-    url: '/location/query',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({'id':id}),
-    success: function(response){
-      console.log(response['latitude']);
-      console.log(response['longitude']);
-      },
-    error: function(response){
-      alert(response)
-      }
-  });
+function get_marker(id) {
+    $.ajax({
+        url: '/location/query',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify({'id': id}),
+        success: function (response) {
+            console.log(response['latitude']);
+            console.log(response['longitude']);
+        },
+        error: function (response) {
+            alert(response)
+        }
+    });
 } 
